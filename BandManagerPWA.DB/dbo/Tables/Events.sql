221d87d0381f45083bb42d6e4f8b5864f5c60b03
Jacob Logue <jakelogue84@gmail.com>
2023-09-30 18:09:47 -0400

    Update dbproj

---
+++ BandManagerPWA.DB/dbo/Tables/Events.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Events] (
+    [Id]          UNIQUEIDENTIFIER NOT NULL,
+    [Title]       NVARCHAR (MAX)   NOT NULL,
+    [Description] NVARCHAR (MAX)   NOT NULL,
+    [Location]    NVARCHAR (MAX)   NOT NULL,
+    [Date]        DATETIME2 (7)    NOT NULL,
+    CONSTRAINT [PK_Events] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
--- BandManagerPWA.DataAccess/Models/ApplicationDbContext.cs
+++ BandManagerPWA.DataAccess/Models/ApplicationDbContext.cs
@@ -6,25 +6,5 @@ namespace BandManagerPWA.DataAccess.Models
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<Event> Events { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-            {
-                var properties = entityType.GetProperties();
-
-                foreach (var property in properties)
-                {
-                    if (property.ClrType == typeof(Guid))
-                    {
-                        modelBuilder.Entity(entityType.Name)
-                                    .Property(property.Name)
-                                    .ValueGeneratedOnAdd();
-                    }
-                }
-            }
-        }
-
     }
 }
