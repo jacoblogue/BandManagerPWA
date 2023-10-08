066e6cabf46c953f4a008d4e920237b603a3f4f5
Jacob Logue <jakelogue84@gmail.com>
2023-10-08 15:37:33 -0400

    Add validation ( doesn't submit correctly)

--- reactapp/src/components/events/CreateEventForm.tsx
+++ reactapp/src/components/events/CreateEventForm.tsx
@@ -1,14 +1,42 @@
 import ExistingEventModel from "@/models/ExistingEventModel";
 import NewEventModel from "@/models/NewEventModel";
 import axios from "axios";
+import { Field, Formik, useFormik } from "formik";
 import React from "react";
-import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import {
+  Button,
+  Form,
+  FormFeedback,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
+import * as Yup from "yup";
 
 interface Props {
   setEvents: React.Dispatch<React.SetStateAction<ExistingEventModel[]>>;
   events: ExistingEventModel[];
   onFormSubmit: () => void;
 }
+const todayAtMidnight = new Date();
+todayAtMidnight.setHours(0, 0, 0, 0);
+const validationSchema = Yup.object({
+  title: Yup.string()
+    .required("Title is required")
+    .min(3, "Title should be at least 3 characters")
+    .max(100, "Title should not exceed 100 characters"),
+  description: Yup.string()
+    .optional()
+    .max(500, "Description should not exceed 500 characters"),
+  date: Yup.date()
+    .required("Date is required")
+    .min(todayAtMidnight, "Event date should not be in the past"),
+  time: Yup.string().required("Time is required"),
+  location: Yup.string()
+    .required("Location is required")
+    .min(3, "Location should be at least 3 characters")
+    .max(100, "Location should not exceed 100 characters"),
+});
 
 export default function CreateEventForm({
   setEvents,
@@ -23,37 +51,13 @@ export default function CreateEventForm({
   });
 
   const createEvent = (newEvent: NewEventModel) => {
-    console.log(newEvent);
-
-    const utcDate = new Date(
-      Date.UTC(
-        newEvent.date.getFullYear(),
-        newEvent.date.getMonth(),
-        newEvent.date.getDate(),
-        newEvent.date.getHours(),
-        newEvent.date.getMinutes()
-      )
-    );
-
-    const preparedEvent = {
-      ...newEvent,
-      date: utcDate,
-    };
-
     axios.post("/api/event", newEvent).then((response) => {
       setEvents([...events, response.data]);
     });
   };
 
-  const handleSubmit = (formEvent: React.FormEvent<HTMLFormElement>) => {
-    formEvent.preventDefault();
-    createEvent(newEvent);
-    onFormSubmit();
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const customHandleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    console.log(name, value);
     setNewEvent((prevState) => {
       // Clone the existing state
       const updatedState: NewEventModel = { ...prevState };
@@ -83,61 +87,143 @@ export default function CreateEventForm({
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <FormGroup>
-        <Label for="title">Title</Label>
-        <Input
-          type="text"
-          name="title"
-          id="title"
-          placeholder="Enter the event title"
-          value={newEvent.title}
-          onChange={handleChange}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="description">Description</Label>
-        <Input
-          type="textarea"
-          name="description"
-          id="description"
-          placeholder="Enter the event description"
-          value={newEvent.description}
-          onChange={handleChange}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="date">Date</Label>
-        <Input
-          type="date"
-          name="date"
-          id="date"
-          placeholder="Enter the event date"
-          onChange={handleChange}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="time">Time</Label>
-        <Input
-          type="time"
-          name="time"
-          id="time"
-          placeholder="Enter the event time"
-          onChange={handleChange}
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="location">Location</Label>
-        <Input
-          type="text"
-          name="location"
-          id="location"
-          placeholder="Enter the event location"
-          value={newEvent.location}
-          onChange={handleChange}
-        />
-      </FormGroup>
-      <Button type="submit">Create Event</Button>
-    </Form>
+    <Formik
+      initialValues={{
+        title: "",
+        description: "",
+        date: new Date(),
+        time: new Date().toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        }),
+        location: "",
+      }}
+      validationSchema={validationSchema}
+      validateOnBlur={true}
+      onSubmit={(values: NewEventModel) => {
+        createEvent(values);
+        onFormSubmit();
+      }}
+    >
+      {(formik) => (
+        <Form>
+          <FormGroup>
+            <Label for="title">Title</Label>
+            <Field name="title">
+              {({ field, meta }) => {
+                return (
+                  <div>
+                    <Input
+                      {...field}
+                      type="text"
+                      name="title"
+                      id="title"
+                      placeholder="Enter the event title"
+                      invalid={meta.touched && meta.error}
+                      valid={meta.touched && !meta.error}
+                    />
+                    <FormFeedback>{meta.error}</FormFeedback>
+                  </div>
+                );
+              }}
+            </Field>
+          </FormGroup>
+          <FormGroup>
+            <Label for="description">Description</Label>
+            <Field name="description">
+              {({ field, meta }) => {
+                return (
+                  <div>
+                    <Input
+                      {...field}
+                      type="textarea"
+                      name="description"
+                      id="description"
+                      placeholder="Enter the event description"
+                      invalid={meta.touched && meta.error}
+                      valid={meta.touched && !meta.error}
+                    />
+                    <FormFeedback>{meta.error}</FormFeedback>
+                  </div>
+                );
+              }}
+            </Field>
+            {formik.errors.description && formik.touched.description ? (
+              <div>{formik.errors.description}</div>
+            ) : null}
+          </FormGroup>
+          <FormGroup>
+            <Label for="date">Date</Label>
+            <Field name="date">
+              {({ field, meta }) => {
+                const dateValue =
+                  field.value instanceof Date
+                    ? field.value.toISOString().substring(0, 10)
+                    : field.value;
+                return (
+                  <div>
+                    <Input
+                      {...field}
+                      type="date"
+                      name="date"
+                      id="date"
+                      invalid={meta.touched && meta.error}
+                      placeholder="Enter the event date"
+                      value={dateValue}
+                      valid={meta.touched && !meta.error}
+                    />
+                    <FormFeedback>{meta.error}</FormFeedback>
+                  </div>
+                );
+              }}
+            </Field>
+          </FormGroup>
+          <FormGroup>
+            <Label for="time">Time</Label>
+            <Field name="time">
+              {({ field, meta }) => {
+                return (
+                  <div>
+                    <Input
+                      {...field}
+                      type="time"
+                      name="time"
+                      id="time"
+                      invalid={meta.touched && meta.error}
+                      placeholder="Enter the event time"
+                      valid={meta.touched && !meta.error}
+                    />
+                    <FormFeedback>{meta.error}</FormFeedback>
+                  </div>
+                );
+              }}
+            </Field>
+          </FormGroup>
+          <FormGroup>
+            <Label for="location">Location</Label>
+            <Field name="location">
+              {({ field, meta }) => {
+                return (
+                  <div>
+                    <Input
+                      {...field}
+                      type="text"
+                      name="location"
+                      id="location"
+                      placeholder="Enter the event location"
+                      invalid={meta.touched && meta.error}
+                      valid={meta.touched && !meta.error}
+                    />
+                    <FormFeedback>{meta.error}</FormFeedback>
+                  </div>
+                );
+              }}
+            </Field>
+          </FormGroup>
+          <Button type="submit">Create Event</Button>
+        </Form>
+      )}
+    </Formik>
   );
 }
