930d23a83ad862b7952acef1b0d3200958d519c7
Jacob Logue <jakelogue84@gmail.com>
2023-10-16 22:18:59 -0400

    Add bottom navbar

--- reactapp/src/components/common/Layout.tsx
+++ reactapp/src/components/common/Layout.tsx
@@ -1,63 +1,74 @@
 import RouteModel from "@/models/RouteModel";
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { Outlet, NavLink } from "react-router-dom";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  Collapse,
-  Nav,
-  NavItem,
-} from "reactstrap";
+import { Navbar, NavbarBrand, Nav, NavItem, Button } from "reactstrap";
 import ThemeToggler from "./ThemeToggler";
+import { useThemeStore } from "@/state/themeStore";
+import { BiPlus } from "react-icons/bi";
 
 interface Props {
   routes: RouteModel[];
 }
 
 export default function Layout({ routes }: Props) {
-  const [collapsed, setCollapsed] = React.useState(true);
+  const { preferredColorScheme } = useThemeStore();
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
   return (
-    <div>
-      {/* Navbar */}
-      <Navbar expand="lg" className="d-flex justify-content-between">
-        <NavbarBrand href="/">
-          <img
-            src="src\assets\BandManagerLogo.svg"
-            alt="BandManager logo"
-            width={"180"}
-            height={"40"}
-            style={{
-              display: "inline-block",
-              marginRight: "5px",
-              verticalAlign: "middle",
-            }}
-          />
-        </NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav className="mr-auto" navbar>
-            {routes.map((route) => (
-              <NavItem key={route.path}>
-                <NavLink
-                  to={route.path}
-                  onClick={() => setCollapsed(!collapsed)}
-                  className={"nav-link"}
-                >
-                  {route.name}
-                </NavLink>
-              </NavItem>
-            ))}
-            <NavItem>
-              <ThemeToggler />
-            </NavItem>
-          </Nav>
-        </Collapse>
+    <div className="d-flex flex-column" style={{ height: "100vh" }}>
+      {/* Top Navbar */}
+      <Navbar color={preferredColorScheme} light expand="lg">
+        <NavbarBrand href="/">BM</NavbarBrand>
+        <Nav className="me-auto" navbar>
+          {routes.map((route) => (
+            <NavItem key={route.name}></NavItem>
+          ))}
+          <NavItem>
+            <ThemeToggler />
+          </NavItem>
+        </Nav>
       </Navbar>
       {/* Main content */}
-      <div className="mt-5">{<Outlet />}</div>
+      <div className="flex-grow-1 mb-5">
+        <Outlet />
+      </div>
+
+      {/* Bottom Navbar */}
+      <Navbar
+        className="border-top"
+        fixed="bottom"
+        color={preferredColorScheme}
+        expand="lg"
+      >
+        <Nav className="w-100 d-flex justify-content-around ">
+          <NavItem className="">
+            <NavLink to="/home" className="nav-link disabled">
+              Home
+            </NavLink>
+          </NavItem>
+          <NavItem className="">
+            {/* Assuming you have some logic to handle adding new events, 
+            you can attach that to the onClick handler of this button */}
+            <Button
+              color={preferredColorScheme}
+              onClick={() => {
+                /* Handle add new event */
+              }}
+            >
+              <BiPlus size={"1.6rem"} />
+            </Button>
+          </NavItem>
+          <NavItem>
+            <NavLink // change to "/events
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "nav-link disabled" : "nav-link"
+              }
+            >
+              Events
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
