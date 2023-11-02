d0f12ea7e997735772a7817044c0393e098c4439
Jacob Logue <jakelogue84@gmail.com>
2023-11-01 20:32:38 -0400

    Add basic style to profile page

--- reactapp/src/components/profile/ProfilePage.tsx
+++ reactapp/src/components/profile/ProfilePage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import styles from "./ProfilePage.module.css";
 import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "../auth/LogoutButton";
 
 interface User {
   name: string;
@@ -20,18 +21,30 @@ const ProfilePage: React.FC = () => {
   }
 
   return (
-    <Container>
+    <Container
+      style={{ height: "100%" }}
+      className="text-center d-flex flex-column justify-content-evenly align-items-center"
+    >
       <h1>Profile Page</h1>
       <Row>
         <Col>
-          <h2>{user.name}</h2>
-        </Col>
-        <Col>
-          <h3>{user.email}</h3>
-        </Col>
-        <Col>
           <img src={user.picture} alt={user.name} />
         </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2>Username</h2>
+          <p>{user.name}</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h3>Email</h3>
+          <p>{user.email}</p>
+        </Col>
+      </Row>
+      <Row>
+        <LogoutButton />
       </Row>
     </Container>
   );
