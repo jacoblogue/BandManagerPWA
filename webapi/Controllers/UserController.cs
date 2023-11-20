67a46329aa9bd6a45825eb544bec4cbd2eef7aca
Jacob Logue <jakelogue84@gmail.com>
2023-11-19 20:21:44 -0500

    Refactor UpsertUser method in UserController.cs

--- webapi/Controllers/UserController.cs
+++ webapi/Controllers/UserController.cs
@@ -22,19 +22,38 @@ namespace webapi.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateUser([FromBody] UserDTO incomingUser)
+        public async Task<IActionResult> UpsertUser([FromBody] UserDTO incomingUser)
         {
             // idempotent - if user exists, return 200
             // if user doesn't exist, create user and return 201
             // if user exists but is different, update user and return 200
-            var user = new User
-            {
-                Id = Guid.NewGuid(),
-                Email = incomingUser.Email,
-            };
 
-            //_context.Users.Add(user);
-            return Ok();
+            try
+            {
+                var existingUser = _context.Users.FirstOrDefault(u => u.Email == incomingUser.Email);
+                if (existingUser != null)
+                {
+                    // Check if user is different
+                    // if user is different, update user
+                    return Ok();
+                }
+                else
+                {
+                    var newUser = new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Email = incomingUser.Email,
+                    };
+                    await _context.Users.AddAsync(newUser);
+                    await _context.SaveChangesAsync();
+                    return CreatedAtAction(nameof(GetUsers), newUser);
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO Add logging etc
+                return StatusCode(500, "An error occurred while processing your request");
+            }
         }
     }
 }
