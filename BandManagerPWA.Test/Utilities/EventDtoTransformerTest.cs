62cffdc9316dcde50f9009da58d58eda0c595cc3
Jacob Logue <jakelogue84@gmail.com>
2024-01-01 19:43:11 -0500

    Add EventDtoTransformer and EventDtoTransformerTest classes

---
+++ BandManagerPWA.Test/Utilities/EventDtoTransformerTest.cs
@@ -0,0 +1,59 @@
+﻿using BandManagerPWA.DataAccess.Models;
+using BandManagerPWA.Utils;
+using BandManagerPWA.Utils.Models;
+
+namespace BandManagerPWA.Test.Utilities
+{
+    [TestClass]
+    public class EventDtoTransformerTest
+    {
+        [TestMethod]
+        public void TransformToDtoTest()
+        {
+            // Arrange
+            var @event = new Event
+            {
+                Date = new DateTimeOffset(2018, 1, 1, 0, 0, 0, TimeSpan.Zero),
+                Description = "Test Description",
+                Id = Guid.NewGuid(),
+                Location = "Test Location",
+                Title = "Test Title"
+            };
+
+            // Act
+            var result = EventDtoTransformer.TransformToDto(@event);
+
+            // Assert
+            Assert.AreEqual(@event.Date, result.Date);
+            Assert.AreEqual(@event.Description, result.Description);
+            Assert.AreEqual(@event.Id, result.Id);
+            Assert.AreEqual(@event.Location, result.Location);
+            Assert.AreEqual(@event.Title, result.Title);
+        }
+
+        [TestMethod]
+        public void TransformToEventTest()
+        {
+            // Arrange
+            var eventDto = new EventDTO
+            {
+                Date = new DateTime(2018, 1, 1),
+                Description = "Test Description",
+                Id = Guid.NewGuid(),
+                Location = "Test Location",
+                Title = "Test Title"
+            };
+
+            var expectedUTCDate = TimeZoneInfo.ConvertTimeToUtc(eventDto.Date);
+            // Act
+            var result = EventDtoTransformer.TransformToEvent(eventDto);
+
+            // Assert
+            Assert.AreEqual(expectedUTCDate, result.Date);
+            Assert.AreEqual(eventDto.Description, result.Description);
+            Assert.AreEqual(eventDto.Id, result.Id);
+            Assert.AreEqual(eventDto.Location, result.Location);
+            Assert.AreEqual(eventDto.Title, result.Title);
+        }
+    }
+}
---
+++ BandManagerPWA.Utils/EventDtoTransformer.cs
@@ -0,0 +1,45 @@
+﻿using BandManagerPWA.DataAccess.Models;
+using BandManagerPWA.Utils.Models;
+
+namespace BandManagerPWA.Utils
+{
+    /// <summary>
+    /// Class responsible for transforming Event objects to EventDTO objects and vice versa.
+    /// </summary>
+    public class EventDtoTransformer
+    {
+        /// <summary>
+        /// Transforms an Event object to an EventDTO object.
+        /// </summary>
+        /// <param name="event">The Event object to transform.</param>
+        /// <returns>The transformed EventDTO object.</returns>
+        public static EventDTO TransformToDto(Event @event)
+        {
+            return new EventDTO
+            {
+                Id = @event.Id,
+                Date = @event.Date.UtcDateTime,
+                Description = @event.Description,
+                Location = @event.Location,
+                Title = @event.Title
+            };
+        }
+
+        /// <summary>
+        /// Transforms an EventDTO object to an Event object.
+        /// </summary>
+        /// <param name="eventDto">The EventDTO object to transform.</param>
+        /// <returns>The transformed Event object.</returns>
+        public static Event TransformToEvent(EventDTO eventDto)
+        {
+            return new Event
+            {
+                Id = eventDto.Id,
+                Date = new DateTimeOffset(eventDto.Date).ToUniversalTime(),
+                Description = eventDto.Description,
+                Location = eventDto.Location,
+                Title = eventDto.Title
+            };
+        }
+    }
+}
