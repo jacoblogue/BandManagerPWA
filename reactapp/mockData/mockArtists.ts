cead2d94b79836aba2cf9147c74078091da02a32
Jacob Logue <jakelogue84@gmail.com>
2024-04-13 13:39:02 -0400

    Add some mock data

---
+++ reactapp/mockData/mockArtists.ts
@@ -0,0 +1,5 @@
+export const artists = [
+  { id: 1, name: "Artist 1" },
+  { id: 2, name: "Artist 2" },
+  { id: 3, name: "Artist 3" },
+];
---
+++ reactapp/mockData/mockSongs.ts
@@ -0,0 +1,17 @@
+export const mockSongs = [
+  {
+    title: "Song 1",
+    artist: "Artist 1",
+    key: "C",
+  },
+  {
+    title: "Song 2",
+    artist: "Artist 2",
+    key: "G",
+  },
+  {
+    title: "Song 3",
+    artist: "Artist 3",
+    key: "D",
+  },
+];
